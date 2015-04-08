1d4ceabd31ad611a124079da13932e06edf2b248
JeremyCraig <jemmies_knights@yahoo.com.au>
2015-04-08 16:54:02 +1000

    Added collision

--- Player.js
+++ Player.js
@@ -5,59 +5,121 @@ var Player = function()
 {
 	this.image = document.createElement("img");
 	
-	this.a = new Vector2();
-	this.xPos = canvas.width/2;
-	this.yPos = canvas.height/2;
-	
 	this.width = 119;
 	this.height = 103;
 	
-	this.velocityX = 0;
-	this.velocityY = 0;
+	this.position = new Vector2();
+	this.position.set = (canvas.width / 2, canvas.height / 2);
+	
+	this.velocity = new Vector2();
 	
 	this.angularVelocity = 0;
-	
 	this.rotation = 0;
-	
-	this.image.src = "hero.png";
+	this.image.src = "hero1.png";
 };
 
 Player.prototype.update = function(deltaTime)
 {
-	if(this.xPos > canvas.width - (this.width / 2))
+	if(this.position.x > canvas.width - (this.width / 2))
 	{
-		this.xPos = canvas.width - (this.width / 2);
+		this.position.x = canvas.width - (this.width / 2);
 	}
-	if(this.xPos < 0 + (this.width / 2))
+	if(this.position.x < 0 + (this.width / 2))
 	{
-		this.xPos = 0 + (this.width / 2);
+		this.position.x = 0 + (this.width / 2);
 	}
-	if(this.yPos > canvas.height - (this.height / 2))
+	if(this.position.y > canvas.height - (this.height / 2))
 	{
-		this.yPos = canvas.height - (this.height / 2);
+		this.position.y = canvas.height - (this.height / 2);
 	}
-	if(this.yPos < 0 + (this.width / 2) - 7)
+	if(this.position.y < 0 + (this.width / 2) - 7)
 	{
-		this.yPos = 0 + (this.width / 2) - 7;
+		this.position.y = 0 + (this.width / 2) - 7;
 	}
 
+	var acceleration = new Vector2();
+	var playerAccel = 20;
+	var playerDrag = 11;
+	var playerGravity = TILE * 9.8 * 6;
+	
+	acceleration.y = playerGravity;
+	
 	if(keyboard.isKeyDown(keyboard.KEY_W) == true)
 	{
-		this.yPos -= 5;
+		acceleration.y -= 5;
 	}
 	if(keyboard.isKeyDown(keyboard.KEY_S) == true)
 	{
-		this.yPos += 5;
+		acceleration.y += 5;
 	}
 	if(keyboard.isKeyDown(keyboard.KEY_A) == true)
 	{
-		this.xPos -= 5;
+		acceleration.x -= 5;
 		flipPlayer = true;
 	}
 	if(keyboard.isKeyDown(keyboard.KEY_D) == true)
 	{
-		this.xPos += 5;
+		acceleration.x += 5;
 		flipPlayer = false;
+	}
+	
+	acceleration = acceleration.subtract(this.velocity.multiplyScalar(playerDrag));
+	
+	this.velocity = this.velocity.add(acceleration.multiplyScalar(deltaTime));
+	this.position = this.position.add(this.velocity.multiplyScalar(deltaTime));
+	
+	var tx = pixelToTile(this.position.x);
+	var ty = pixelToTile(this.position.y);
+	
+	var nx = this.position.x % TILE;
+	var ny = this.position.y % TILE;
+	
+	var cell = cellAtTileCoord(LAYER_PLATFORMS, tx, ty);
+	var cell_right = cellAtTileCoord(LAYER_PLATFORMS, tx+1, ty);
+	var cell_down = cellAtTileCoord(LAYER_PLATFORMS, tx, ty+1);
+	var cell_diag = cellAtTileCoord(LAYER_PLATFORMS, tx+1, ty+1);
+	
+	//Collision
+	if ( this.velocity.y > 0 )
+	{
+		if ( (cell_down && !cell) || (cell_diag && !cell_right && nx) )
+		{
+			this.position.y = tileToPixel(ty);
+			this.velocity.y = 0;
+			ny = 0;
+		}
+	}
+	else if (this.velocity.y < 0 ) //if moving up
+	{
+		if ( (cell && !cell_down) || (cell_right && !cell_diag && nx) )
+		{
+			this.position.y = tiletoPixel(ty + 1);
+			this.velocity.y = 0;
+			
+			cell = cell_down;
+			cell_right = cell_diag;
+			
+			cell_down = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 2);
+			cell_diag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 2);
+			
+			ny = 0;
+		}
+	}
+	if (this.velocity.x > 0 )//if we're moving right
+	{
+		if ( (cell_right && !cell) || (cell_diag && !cell_down && ny) )
+		{
+			this.position.x = tileToPixel(tx);
+			this.velocity.x = 0;
+		}
+	}
+	else if (this.velocity.x < 0) //if we're moving left
+	{
+		if ( (cell && !cell_right) || (cell_down && !cell_diag && ny) )
+		{
+			this.position.x = tileToPixel(tx+1);
+			this.velocity.x = 0;
+		}
 	}
 }
 
@@ -67,13 +129,13 @@ Player.prototype.draw = function()
 	{
 		context.save();
 			context.translate(this.width, 0);
-			context.translate(this.xPos, this.yPos);
+			context.translate(this.position.x, this.position.y);
 			context.scale(-1, 1);
 			context.drawImage(this.image, +this.width/2, -this.height/2);
 		context.restore();
 	}else{
 		context.save();
-			context.translate(this.xPos, this.yPos);
+			context.translate(this.position.x, this.position.y);
 			context.rotate(this.rotation);
 			context.drawImage(this.image, -this.width/2, -this.height/2);
 		context.restore();
--- Vector2.js
+++ Vector2.js
@@ -2,46 +2,50 @@ var canvas = document.getElementById("gameCanvas");
 
 var Vector2 = function() 
 {
-	this.xPos = canvas.width/2;
-	this.yPos = canvas.height/2;
+	this.x = canvas.width/2;
+	this.y = canvas.height/2;
 };
 
 Vector2.prototype.length = function()
 {
-	var length = Math.sqrt(Math.pow(this.xPos, 2) + Math.pow(this.yPos, 2));
+	var length = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
 	return length;
 }
 
 Vector2.prototype.setPos = function(x, y)
 {
-	this.xPos = x;
-	this.yPos = y;
+	this.x = x;
+	this.y = y;
 }
 
 Vector2.prototype.normalize = function()
 {
-	this.xPos = this.xPos / Vector2.length;
-	this.yPos = this.yPos / Vector2.length;
+	this.x = this.x / Vector2.length;
+	this.y = this.y / Vector2.length;
 }
 
-Vector2.prototype.add = function(V2)
+Vector2.prototype.add = function(x, y)
 {
 	var result = new Vector2();
 	
-	result.xPos = this.xPos + V2.x
-	result.yPos = this.yPos + V2.y
+	result.x = this.x + x
+	result.y = this.y + y
 	
 	return result;
 }
 
-Vector2.prototype.subtract = function(x2, y2)
+Vector2.prototype.subtract = function(x, y)
 {
-	this.xPos -= x2;
-	this.yPos -= y2;
+	var result = new Vector2();
+	
+	result.x = this.x - x
+	result.y = this.y - y
+	
+	return result;
 }
 
 Vector2.prototype.multiplyScalar = function(num)
 {
-	this.xPos = this.xPos * num;
-	this.yPos = this.yPos * num;
+	this.x = this.x * num;
+	this.y = this.y * num;
 }
